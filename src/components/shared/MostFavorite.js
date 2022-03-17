3712ca01c6c6ae497f638b4c757dd00719055e67
DoMinhHieu1997 <36026729+DoMinhHieu1997@users.noreply.github.com>
2022-03-17 14:58:58 +0700

    bingding top like post data

--- src/components/shared/MostFavorite.js
+++ src/components/shared/MostFavorite.js
@@ -1,16 +1,20 @@
 import PostItem from "./PostItem";
 import { useState,useEffect } from "react";
 import SkeletonItem from "./SkeletonItem";
-import { SvgIcon } from "@mui/material";
+import COMMON from "../Common";
 
 const MostFavorite = () => {
     const [isLoading, setIsLoading] = useState(false);
+    const [topLike, setTopLike] = useState('');
 
     useEffect(() => {
-        // setIsLoading(true);
-        // setTimeout(() => {
-        //     setIsLoading(false);
-        // },3000);
+        setIsLoading(true);
+        fetch(`${COMMON.DOMAIN}posts/toplike`)
+        .then((res) => res.json())
+        .then((resJson) => {
+            setTopLike(resJson.data);
+            setIsLoading(false);
+        });
     },[]);
 
     return <div>
@@ -18,14 +22,12 @@ const MostFavorite = () => {
         <div className="w-75 mx-auto mt-3" style={{height: "2px", backgroundColor:"black"}}></div>
         <div className="mt-4 row">
             {
-                isLoading
-                ? <>
-                    <div><PostItem /></div>
-                    <div><PostItem /></div>
-                    <div><PostItem /></div>
-                </>
-                :
-                <>
+                topLike && topLike.map((item) => {
+                    return <div className="mb-3"><PostItem data={item} isTopLikeItem={true}/></div>
+                })
+            }
+            {
+                isLoading && <>
                     <SkeletonItem type="1"/>
                     <SkeletonItem type="1"/>
                     <SkeletonItem type="1"/>
